eb22c48ea201eb1e6cb04026fdaaaafb1549ed54
Rizallazis <rizalbyi03@gmail.com>
2026-05-27 23:37:42 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Menggunakan '/' karena Anda menggunakan custom domain utama (https://rizalazis.xyz/)
-})
