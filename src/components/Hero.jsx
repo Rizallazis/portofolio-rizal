f492e26c0e31d6477b60d89923f9ecbfabb9089a
Rizallazis <rizalbyi03@gmail.com>
2026-05-28 01:13:31 +0700

    fix favicon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Hero() {
-  const [typedText, setTypedText] = useState('');
+  const [typedText, setTypedText] = useState('Psychology Graduate');
 
   useEffect(() => {
     // Dynamic greeting based on time
