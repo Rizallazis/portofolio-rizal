6193d06b147d9e3a9cbb0159a82ef4a103489e24
Rizallazis <rizalbyi03@gmail.com>
2026-05-27 23:28:04 +0700

    Add base URL for custom domain in vite.config.js
    Set base URL for custom domain in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Menggunakan '/' karena Anda menggunakan custom domain utama (https://rizalazis.xyz/)
 })
